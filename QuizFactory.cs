d35e7559e8c46d197c08a6b0e8dc2625ec271f14
Amanda <amanda.baum01@gmail.com>
2020-07-26 08:57:25 -0400

    reading Country Capital information from csv file

--- QuizFactory.cs
+++ QuizFactory.cs
@@ -12,96 +12,41 @@ namespace QuizFactory
 
         public static List<PromptAnswer> quizData = new List<PromptAnswer>{};
 
-        public static List<PromptAnswer> CountryData = new List<PromptAnswer> {
-            new PromptAnswer("Afghanistan", "Kabul"),
-            new PromptAnswer("Albaia", "Tirana"),
-            new PromptAnswer("Algeria", "Algiers"),
-            new PromptAnswer("Andorra", "Andorra La Vella"),
-            new PromptAnswer("Angola", "Luanda"),
-            new PromptAnswer("Antigua & Barbuda", "Saint Johns"),
-            new PromptAnswer("Argentina", "Buenos Aires"),
-            new PromptAnswer("Armenia", "Yerevan"),
-            new PromptAnswer("Australia", "Canberra"),
-            new PromptAnswer("Austria", "Vienna"),
-            new PromptAnswer("Azerbaijan", "Baku"),
-            new PromptAnswer("Bahamas", "Nassau"),
-            new PromptAnswer("Bahrain", "Manama"),
-            new PromptAnswer("Bangladesh", "Bridgetown"),
-            new PromptAnswer("Belarus", "Minsk"),
-            new PromptAnswer("Belgium", "Brussels"),
-            new PromptAnswer("Belize", "Belmopan"),
-            new PromptAnswer("Benin", "Porto-Novo"),
-            new PromptAnswer("Bhutan", "Thimphu"),
-        };
+        // public static List<PromptAnswer> CountryData = new List<PromptAnswer> {
+        //     new PromptAnswer("Afghanistan", "Kabul"),
+        //     new PromptAnswer("Albaia", "Tirana"),
+        //     new PromptAnswer("Algeria", "Algiers"),
+        //     new PromptAnswer("Andorra", "Andorra La Vella"),
+        //     new PromptAnswer("Angola", "Luanda"),
+        //     new PromptAnswer("Antigua & Barbuda", "Saint Johns"),
+        //     new PromptAnswer("Argentina", "Buenos Aires"),
+        //     new PromptAnswer("Armenia", "Yerevan"),
+        //     new PromptAnswer("Australia", "Canberra"),
+        //     new PromptAnswer("Austria", "Vienna"),
+        //     new PromptAnswer("Azerbaijan", "Baku"),
+        //     new PromptAnswer("Bahamas", "Nassau"),
+        //     new PromptAnswer("Bahrain", "Manama"),
+        //     new PromptAnswer("Bangladesh", "Bridgetown"),
+        //     new PromptAnswer("Belarus", "Minsk"),
+        //     new PromptAnswer("Belgium", "Brussels"),
+        //     new PromptAnswer("Belize", "Belmopan"),
+        //     new PromptAnswer("Benin", "Porto-Novo"),
+        //     new PromptAnswer("Bhutan", "Thimphu"),
+        // };
 
-        public static List<PromptAnswer> StateData = new List<PromptAnswer>
-        {
-                new PromptAnswer("Alabama", "Montgomery"),
-                new PromptAnswer("Alaska", "Juneau"),
-                new PromptAnswer("Arizona", "Phoenix"),
-                new PromptAnswer("Arkansas", "Little Rock"),
-                new PromptAnswer("California", "Sacramento"),
-                new PromptAnswer("Colorado", "Denver"),
-                new PromptAnswer("Connecticut", "Hartford"),
-                new PromptAnswer("Deleware", "Dover"),
-                new PromptAnswer("Florida", "Tallahassee"),
-                new PromptAnswer("Georgia", "Atlanta"),
-                new PromptAnswer("Hawaii", "Honolulu"),
-                new PromptAnswer("Idaho", "Bose"),
-                new PromptAnswer("Illinois", "Springfield"),
-                new PromptAnswer("Indiana", "Indianapolis"),
-                new PromptAnswer("Iowa", "Des Moines"),
-                new PromptAnswer("Kansas", "Topeka"),
-                new PromptAnswer("Kentucky", "Frankfort"),
-                new PromptAnswer("Louisiana", "Baton Rouge"),
-                new PromptAnswer("Maine", "Augusta"),
-                new PromptAnswer("Maryland", "Annapolis"),
-                new PromptAnswer("Massachusetts", "Boston"),
-                new PromptAnswer("Michigan", "Lansing"),
-                new PromptAnswer("Minnesota", "Saint Paul"),
-                new PromptAnswer("Mississippi", "Jackson"),
-                new PromptAnswer("Missouri", "Jefferson City"),
-                new PromptAnswer("Montana", "Helena"),
-                new PromptAnswer("Nebraska", "Lincoln"),
-                new PromptAnswer("Nevada", "Carson city"),
-                new PromptAnswer("New Hampshire", "Concord"),
-                new PromptAnswer("New Jersey", "Trenton"),
-                new PromptAnswer("New Mexico", "Santa Fe"),
-                new PromptAnswer("New York", "Albany"),
-                new PromptAnswer("North Carolina", "Raleigh"),
-                new PromptAnswer("North Dakata", "Bismarck"),
-                new PromptAnswer("Ohio", "Columbus"),
-                new PromptAnswer("Oklahoma", "Oklahoma City"),
-                new PromptAnswer("Oregon", "Salem"),
-                new PromptAnswer("Pennsylvania", "Harrisburg"),
-                new PromptAnswer("Rhode Island", "Providence"),
-                new PromptAnswer("South Carolina", "Columbia"),
-                new PromptAnswer("South Dakota", "Pierre"),
-                new PromptAnswer("Tennessee", "Nashville"),
-                new PromptAnswer("Texas", "Austin"),
-                new PromptAnswer("Urah", "Salt Lake City"),
-                new PromptAnswer("Vermont", "Montpelier"),
-                new PromptAnswer("Virginia", "Richmond"),
-                new PromptAnswer("Washington", "Olympia"),
-                new PromptAnswer("West Virginia", "Charleston"),
-                new PromptAnswer("Wisconsin", "Madison"),
-                new PromptAnswer("Wyoming", "Cheyenne")
-        };
         public static void GenerateQuiz(int quizType)
         {   
             // List<PromptAnswer> quizData = new List<PromptAnwser>();
             // if 1 generate state capital
             if (quizType == 1)
             {
-                // sets quizData to List<PromptAnser> for the State/City information
+                // call a method to read StatesCapitals csv file
                 ReadQuizData("./StatesCapitals.csv");
             }
             else if(quizType == 2)
             {
-                // sets quizData to List<PromptAnswer> for the Country/Capital information
-                // quizData = CountryData;
+                ReadQuizData("./CountryCapital.csv");
             }
-            // Returns list<PromptAnser> {state/country, {capitol, IsCorrect}}
         }
 
         public static void ReadQuizData(string fileName)
